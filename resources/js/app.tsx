ce7bdd564f9fef082f6e4914f173689e7b289377
Tristan <tristanbudd@outlook.com>
2026-05-28 17:22:31 +0100

    Mass Update Packages & Fix app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -17,15 +17,25 @@ router.on('navigate', (event) => {
     });
 });
 
-const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
+interface PageModule {
+    default: React.ComponentType;
+}
+
+const pages = import.meta.glob<PageModule>('./Pages/**/*.tsx', { eager: true });
+
+const el = document.getElementById('app');
+const initialPage = el?.dataset.page ? JSON.parse(el.dataset.page) : undefined;
 
 createInertiaApp({
     title: (title) => (title ? `${appName} | ${title}` : appName),
+    page: initialPage,
 
     resolve: (name) => {
         const page = pages[`./Pages/${name}.tsx`];
-        if (!page) console.error(`Page Not Found | No page component found for: ${name}`);
-        return page;
+        if (!page) {
+            throw new Error(`Page Not Found | No page component found for: ${name}`);
+        }
+        return page.default || page;
     },
 
     setup({ el, App, props }) {
